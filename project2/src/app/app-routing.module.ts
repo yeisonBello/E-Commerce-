05d18c2f58ae2932646fa267bd5f23c7770a06ec
yeison <yeisonbello.c@gmail.com>
2022-06-24 12:00:03 -0500

    first commit

---
+++ project2/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './component/cart/cart.component';
+import { ProductsComponent } from './component/product/product.component'; 
+
+const routes: Routes = [
+  {path:'', redirectTo:'products',pathMatch:'full'},
+  {path:'product', component: ProductsComponent},
+  {path:'cart', component: CartComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
